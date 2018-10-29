5c6c4c568d01f082a9b52022b8c3c83bf30fea1a
Vittorio Mignini <vittorio@localhost.localdomain>
2018-10-29 23:12:00 +0100

    update 29-10-18 23:11

--- main.c
+++ main.c
@@ -66,91 +66,21 @@ int main(){
   //printbitseq(text);
 
   for(int m=0; m<4; m++){
-    Cube cube, cubeappo;
     //SHIFT
-    /*for(int ii=0; ii<0; ii++){
-      for(int i=0; i<4; i++){
-        for(int q=0; q<4; q++){
-          for(int z=0; z<4; z++){
-            for(int p=0; p<4; p++){
-              if(i==0){
-                  cubeappo[q][z][p].b = text[0][q][z][p].b;
-              }
-              if(i<3){
-                  cube[q][z][p].b = text[i+1][q][z][p].b;
-                  text[i][q][z][p].b = cube[q][z][p].b;
-              }else{
-                  text[i][q][z][p].b = cubeappo[q][z][p].b;
-              }
-            }
-          }
-        }
-      }
-    }*/
-    /****************/
     shiftcubes(text, m, true);
+    /****************/
 
     for(int i=0; i<4; i++){
-      
-      // matrix, matrixappo;
       //SHIFT
-      /*for(int ii=0; ii<0; ii++){
-        for(int q=0; q<4; q++){
-          for(int z=0; z<4; z++){
-            for(int p=0; p<4; p++){
-              if(q==0){
-                  matrixappo[z][p].b = text[i][0][z][p].b;
-              }
-              if(q<3){
-                  matrix[z][p].b = text[i][q+1][z][p].b;
-                  text[i][q][z][p].b = matrix[z][p].b;
-              }else{
-                  text[i][q][z][p].b = matrixappo[z][p].b;
-              }
-            }
-          }
-        }
-      }*/
-      /****************/
       shiftmatrix(text, i, true);
-      
+      /****************/
       for(int j=0; j<4; j++){
-        
-        //Vector vector, vectorappo;
-        /*for(int ii=0; ii<0; ii++){
-          for(int z=0; z<4; z++){
-            for(int p=0; p<4; p++){
-              if(z==0){
-                  vectorappo[p].b = text[i][j][0][p].b;
-              }
-              if(z<3){
-                  vector[p].b = text[i][j][z+1][p].b;
-                  text[i][j][z][p].b = vector[p].b;
-              }else{
-                  text[i][j][z][p].b = vectorappo[p].b;
-              }
-            }
-          }
-        }*/
+        //SHIFT
         shiftrows(text, i,j, true);
-     
+        /****************/
         for(int x=0; x<4; x++){
           
-          //Bit bit, bitappo;
-          //SHIFT
-          /*for(int ii=0; ii<0; ii++){
-            for(int p=0; p<4; p++){
-              if(p==0){
-                  bitappo.b = text[i][j][x][0].b;
-              }
-              if(p<3){
-                  bit.b = text[i][j][x][p+1].b;
-                  text[i][j][x][p].b = bit.b;
-              }else{
-                  text[i][j][x][p].b = bitappo.b;
-              }
-            }
-          }*/
+         
           /****************/
           shiftbits(text, i,j,x, true);
 
@@ -255,92 +185,21 @@ int main(){
             text[i][j][x][y].b ^= key[i][j][x][y].b;
 
           }
-          
-          //Bit bit, bitappo;
-          //SHIFT
-          /*for(int ii=0; ii<0; ii++){
-            for(int p=0; p<4; p++){
-              if(p==0){
-                  bitappo.b = text[i][j][x][0].b;
-              }
-              if(p<3){
-                  bit.b = text[i][j][x][p+1].b;
-                  text[i][j][x][p].b = bit.b;
-              }else{
-                  text[i][j][x][p].b = bitappo.b;
-              }
-            }
-          }*/
+
           
           /****************/
         shiftbits(text, i,j,x, false);
         }
 
-        //Vector vector, vectorappo;
-        //SHIFT
-        /*for(int ii=0; ii<0; ii++){
-          for(int z=0; z<4; z++){
-            for(int p=0; p<4; p++){
-              if(z==0){
-                  vectorappo[p].b = text[i][j][0][p].b;
-              }
-              if(z<3){
-                  vector[p].b = text[i][j][z+1][p].b;
-                  text[i][j][z][p].b = vector[p].b;
-              }else{
-                  text[i][j][z][p].b = vectorappo[p].b;
-              }
-            }
-          }
-        }*/
+    
         shiftrows(text, i, j, false);
       }
       /****************/
-      //Matrix matrix, matrixappo;
-      //SHIFT
-      /*for(int ii=0; ii<0; ii++){
-        for(int q=0; q<4; q++){
-          for(int z=0; z<4; z++){
-            for(int p=0; p<4; p++){
-              if(q==0){
-                  matrixappo[z][p].b = text[i][0][z][p].b;
-              }
-              if(q<3){
-                  matrix[z][p].b = text[i][q+1][z][p].b;
-                  text[i][q][z][p].b = matrix[z][p].b;
-              }else{
-                  text[i][q][z][p].b = matrixappo[z][p].b;
-              }
-            }
-          }
-        }
-      }*/
+   
       /****************/
       shiftmatrix(text, i, false);
     }
     
-    //Cube cube, cubeappo;
-      
-    //SHIFT
-    /*for(int ii=0; ii<0; ii++){
-      for(int i=0; i<4; i++){
-        for(int q=0; q<4; q++){
-          for(int z=0; z<4; z++){
-            for(int p=0; p<4; p++){
-              if(i==0){
-                  cubeappo[q][z][p].b = text[0][q][z][p].b;
-              }
-              if(i<3){
-                  cube[q][z][p].b = text[i+1][q][z][p].b;
-                  text[i][q][z][p].b = cube[q][z][p].b;
-              }else{
-                  text[i][q][z][p].b = cubeappo[q][z][p].b;
-              }
-            }
-          }
-        }
-      }
-    }*/
     /****************/
     shiftcubes(text, m, false);
   }
